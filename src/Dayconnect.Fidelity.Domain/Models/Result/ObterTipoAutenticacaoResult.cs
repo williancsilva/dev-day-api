9b1feef19fab3073dc03338d19a2760fbaf2e9e7
Desenv - Ciro Batista <ciro.batista@bancodaycoval.com.br>
2022-05-02 11:05:46 -0300

    ajusta para aceitar autenticacao via AD

---
+++ src/Dayconnect.Fidelity.Domain/Models/Result/ObterTipoAutenticacaoResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Dayconnect.Fidelity.Domain.Models.Result
+{
+    public class ObterTipoAutenticacaoResult
+    {
+        public IEnumerable<FatorAutenticacao> MutiploFatorAutenticacao { get; set; }
+
+    }
+    public class FatorAutenticacao
+    {
+        public int TipoAutenticacao { get; set; }
+    }
+}
---
+++ src/Dayconnect.Fidelity.Domain/Models/Signature/ObterTipoAutenticacaoSignature.cs
@@ -0,0 +1,14 @@
+﻿namespace Dayconnect.Fidelity.Domain.Models.Signature
+{
+    public class ObterTipoAutenticacaoSignature
+    {
+        public ObterTipoAutenticacaoSignature(string _login)
+        {
+            CodSistema = 353;
+            Login = _login;
+        }
+
+        public int CodSistema { get; }
+        public string Login { get; }  
+    }
+}
