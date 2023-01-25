b3328ac6afb124387525fff879b75ed832d4eb63
Willian Cândido da Silva <williancsilva@gmail.com>
2023-01-25 11:38:17 -0300

    Métodos para criptografia

---
+++ src/Dayconnect.BackOffice.Domain/Models/Hash.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace DevSecOps.BackOffice.Domain.Models
+{
+    public class Hash
+    {
+        const int keySize = 64;
+        const int iterations = 350000;
+        HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA512;
+
+        string HashPasword(string password, out byte[] salt)
+        {
+            salt = RandomNumberGenerator.GetBytes(keySize);
+
+            var hash = Rfc2898DeriveBytes.Pbkdf2(
+                Encoding.UTF8.GetBytes(password),
+                salt,
+                iterations,
+                hashAlgorithm,
+                keySize);
+            return Convert.ToHexString(hash);
+        }
+
+        bool VerifyPassword(string password, string hash, byte[] salt)
+        {
+            var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, iterations, hashAlgorithm, keySize);
+            return hashToCompare.SequenceEqual(Convert.FromHexString(hash));
+        }
+    }
+}
