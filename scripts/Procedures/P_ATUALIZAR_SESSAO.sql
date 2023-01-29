b0a215827e1b88d89ee88c1a96c50e68069de1d4
Willian Cândido da Silva <williancsilva@gmail.com>
2023-01-29 10:30:59 -0300

    Novo parametro

--- scripts/Procedures/P_ATUALIZAR_SESSAO.sql
+++ scripts/Procedures/P_ATUALIZAR_SESSAO.sql
@@ -1,6 +1,5 @@
-CREATE PROCEDURE P_ATUALIZAR_SESSAO @DayId INT
+ALTER PROCEDURE P_ATUALIZAR_SESSAO @DayId INT, @isAuthenticated BIT
 AS
 BEGIN
-UPDATE SESSAO SET isAuthenticated = 1 WHERE id = @DayId
+UPDATE SESSAO SET isAuthenticated = @isAuthenticated WHERE id = @DayId
 END
-GO
