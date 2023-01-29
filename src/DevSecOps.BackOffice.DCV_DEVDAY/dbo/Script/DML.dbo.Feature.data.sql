e215d24f12cf21fad8768a9072b3ef366211938a
Angelo Vinicius N. Comar <vinicomar@gmail.com>
2023-01-29 12:07:33 -0300

    Adiciona role de consultar na controller, e script dml

--- src/DevSecOps.BackOffice.DCV_DEVDAY/dbo/Script/DML.dbo.Feature.data.sql
+++ src/DevSecOps.BackOffice.DCV_DEVDAY/dbo/Script/DML.dbo.Feature.data.sql
@@ -1,4 +1,6 @@
-﻿USE [DCV_DEVDAY]
+﻿USE [DCV_DEVDAY] 
+GO 
+
 SET IDENTITY_INSERT [dbo].[Features] ON
 INSERT INTO [dbo].[Features] ([id], [nome], [descricao]) VALUES (4, N'Bloquear', N'Permite que o usuário inativar clientes')
 SET IDENTITY_INSERT [dbo].[Features] OFF
